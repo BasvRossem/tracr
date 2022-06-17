5496256a75558d334008a9726658b05d141dc2d3
Bas van Rossem <31800724+BasvRossem@users.noreply.github.com>
2022-06-17 21:41:16 +0200

    reactor: turn into single return statement

--- Front End/src/service-worker.js
+++ Front End/src/service-worker.js
@@ -24,7 +24,7 @@ precacheAndRoute(self.__WB_MANIFEST);
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+const fileExtensionRegexp = '/[^/?]+\\.[^/]+$';
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }) => {
@@ -36,12 +36,9 @@ registerRoute(
     if (url.pathname.startsWith('/_')) {
       return false;
     } // If this looks like a URL for a resource, because it contains // a file extension, skip.
-
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    } // Return true to signal that we want to use the handler.
-
-    return true;
+    
+    // Return if we want to use the handler.
+    return !(url.pathname.match(fileExtensionRegexp));
   },
   createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
