778baf5f4df21d17a1207353ecc84dbae42b7f4d
Bas van Rossem <bas.v.rossem@gmail.com>
2022-10-26 12:42:25 +0200

    fix: stand-up date is selected date

--- Front End/src/components/Logs/SpeedDial.tsx
+++ Front End/src/components/Logs/SpeedDial.tsx
@@ -4,6 +4,7 @@ import AccessibilityNewIcon from '@mui/icons-material/AccessibilityNew';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import MuiSpeedDial from '@mui/material/SpeedDial';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
+import store from '../../data/store';
 
 interface SpeedDialProps {
   setLog: React.Dispatch<any>;
@@ -15,10 +16,11 @@ export function SpeedDial(props: SpeedDialProps) {
     {
       name: "Stand-up",
       onClick: () => {
+        const date = new Date(store.getState().currentDate.value);
         props.setLog({
           title: "BLT-1817",
-          startTime: moment().startOf("day").set("hours", 10).toDate(),
-          stopTime: moment().startOf("day").set("hours", 10).set("minutes", 30).toDate(),
+          startTime: moment(date).startOf("day").set("hours", 10).toDate(),
+          stopTime: moment(date).startOf("day").set("hours", 10).set("minutes", 30).toDate(),
           notes: ("Stand-up")
         });
         props.openCreateModal();
