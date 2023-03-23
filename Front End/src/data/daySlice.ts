16c7b27ad340d05d9362d3a82f2d683241bceb36
Bas van Rossem <31800724+BasvRossem@users.noreply.github.com>
2023-03-23 21:06:41 +0100

    refactor: unify creation of request options

--- Front End/src/data/daySlice.ts
+++ Front End/src/data/daySlice.ts
@@ -2,7 +2,6 @@ import { createSlice } from '@reduxjs/toolkit';
 import { logApiDate } from '../utils/time';
 import { DateString, ILogDay, LogDay } from '../types';
 
-
 export const logSlice = createSlice({
   name: 'logSlice',
   initialState: {
@@ -21,13 +20,18 @@ export const logSlice = createSlice({
 
 // Action creators are generated for each case reducer function
 export const { setDay } = logSlice.actions;
+export default logSlice.reducer;
 
-const getHeaders = () => {
+function makeRequestOptions(method: "GET" | "POST", body?: string): RequestInit {
   return {
-    'Content-Type': 'application/json', 
-    'Authorization': sessionStorage.getItem("token")
-  } 
-};
+    method,
+    headers: {
+      'Content-Type': 'application/json', 
+      'Authorization': sessionStorage.getItem("token")
+    },
+    body: body as unknown as BodyInit
+  }
+}
 
 function prepIncomingData(days: LogDay[]): LogDay | undefined {
   days.forEach(day => {
@@ -39,10 +43,7 @@ function prepIncomingData(days: LogDay[]): LogDay | undefined {
 export const getDay = (payload: DateString) => dispatch => {
   console.info("Getting logs for selected date", payload);
 
-  const requestOptions = {
-    method: 'GET',
-    headers: getHeaders(),
-  };
+  const requestOptions = makeRequestOptions('GET');
   
   const uri = `${process.env.REACT_APP_BACKEND_URI}/tracr/logs?date=${payload}`;
   fetch(uri, requestOptions)
@@ -57,11 +58,7 @@ export const getDay = (payload: DateString) => dispatch => {
 export const updateDay = (payload: LogDay) => dispatch => {
   console.info("Updating one log with", JSON.stringify(payload));
 
-  const requestOptions = {
-    method: 'POST',
-    headers: getHeaders(),
-    body: JSON.stringify(payload) as unknown as BodyInit
-  };
+  const requestOptions = makeRequestOptions("POST", JSON.stringify(payload));
 
   fetch(`${process.env.REACT_APP_BACKEND_URI}/tracr/logs`, requestOptions)
     .then(res => res.status !== 403 ? res : Promise.reject("Unauthorized"))
@@ -70,14 +67,9 @@ export const updateDay = (payload: LogDay) => dispatch => {
 };
 
 export const getHealth = () => {
-  const requestOptions = {
-    method: 'Get',
-    headers: { 'Content-Type': 'application/json' },
-  };
+  const requestOptions = makeRequestOptions("GET");
 
   const uri = `${process.env.REACT_APP_BACKEND_URI}/tracr/health`;
   fetch(uri, requestOptions)
     .catch(err => console.error(err));
 }
-
-export default logSlice.reducer;
